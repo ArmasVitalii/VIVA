05b3855a7cf590b68343171aba1d3fa4e1ceedb5
Isabela0524 <elena.ciutacu@student.unitbv.ro>
2024-11-01 21:26:26 +0200

    Added Card.h

---
+++ Eter/Eter/Card.h
@@ -0,0 +1,13 @@
+#pragma once
+class Card
+{
+private:
+	//TOBE: CONTINUED
+	int					m_value;
+	bool				m_whoPlayed;
+public:
+	Card(int value, bool whoPlayer);
+	bool				operator==(const Card& other) const;
+	const int& getValue() const;
+	const bool& getWhoPlayed() const;
+};
