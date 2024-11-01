fd78debd6d40051a3dd087f464e5fa3394146731
ArmasVitalii <70914526+ArmasVitalii@users.noreply.github.com>
2024-11-01 19:52:21 +0200

    Added minimal required libraries and demo of a hash structure

--- Eter/Eter/Game.h
+++ Eter/Eter/Game.h
@@ -1,1 +1,18 @@
 #pragma once
+
+#include <iostream>
+#include <vector>
+#include <stack>
+
+
+using gameboard = std::vector<std::vector<std::stack<Card>>>;
+struct pairHash
+{
+	std::size_t operator()(const std::pair<int, int>& p) const
+	{
+		std::size_t h1 = std::hash<int>{}(p.first);
+		std::size_t h2 = std::hash<int>{}(p.second);
+		return h1 ^ (h2 << 1);
+	}
+
+};
