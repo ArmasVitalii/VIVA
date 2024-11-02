2705a1f716cbbcee97a6ff9d8da5290b6d4fe1dd
Isabela0524 <elena.ciutacu@student.unitbv.ro>
2024-11-02 14:02:22 +0200

    Added Card.cpp

---
+++ Eter/Eter/Card.cpp
@@ -0,0 +1,18 @@
+#include "Card.h"
+
+Card::Card(int value, bool whoPlayer) :m_value(value), m_whoPlayed(whoPlayer) {}
+
+bool Card::operator==(const Card& other) const
+{
+	return (m_value == other.m_value) && (m_whoPlayed == other.m_whoPlayed);
+}
+
+const int& Card::getValue() const
+{
+	return m_value;
+}
+
+const bool& Card::getWhoPlayed() const
+{
+	return m_whoPlayed;
+}
