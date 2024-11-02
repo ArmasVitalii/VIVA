58003c1a5a8e66ea9ffc138eb5c9a26bd010c45b
Isabela0524 <elena.ciutacu@student.unitbv.ro>
2024-11-02 14:13:36 +0200

    Added Player.h

---
+++ Eter/Eter/Player.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <vector>
+#include <iostream>
+#include <list>
+#include "Card.h"
+
+class Player
+{
+private:
+	bool						m_playerNumber;
+	std::list<int>				m_playerCards;				//RETHINK FOR LIST OF CARDS
+	std::vector<int>			m_columnVector{ 0,0,0 };
+	std::vector<int>			m_rowVector{ 0,0,0 };	//RETHINK/ FIND WAY FOR 4X4, MAYBE SET FUNCTION
+public:
+	Player(bool playerNumber);		//, const std::list<Card>& playerCards = {});
+
+	std::list<int>& getCards();
+	void						addCard(const Card& card);
+	bool						removeCard(const Card& card);//MAYBE WITH NUMBERS, NOT CARDS
+
+	bool& getPlayerNumber();
+	std::vector<int>			getColumnVector();
+	std::vector<int>			getRowVector();
+
+
+	void						printPlayableCards() const;
+};
+
