552a1fdd8b822da2c22283250eff674163665245
Adriana-Huruba <adriana.huruba@student.unitbv.ro>
2024-11-03 17:53:26 +0200

    Added content to Game.cpp

--- Eter/Eter/Game.cpp
+++ Eter/Eter/Game.cpp
@@ -19,3 +19,30 @@ const Player& Game::getPlayer2() const
 {
 	return m_players[2];
 }
+
+std::pair<int, int> Game::getGridMiddle()
+{
+	return m_gridMiddle;
+}
+
+void Game::printGameboard()
+{
+	for (int i = 0; i < m_gameBoard.size(); ++i)
+	{
+		for (int j = 0; j < m_gameBoard.size(); ++j)
+		{
+			const Card& topCard = m_gameBoard[i][j].top();
+			if (m_gameBoard[i][j].top().getValue() == -1)
+			{
+				std::cout << "( , ) ";
+			}
+			else
+			{
+				std::cout << "(" << topCard.getValue() << "," << topCard.getWhoPlayed() + 1 << ") ";
+			}
+		}
+		std::cout << std::endl;
+	}
+}
+
+
