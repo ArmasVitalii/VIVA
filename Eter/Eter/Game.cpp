a46c1b12288ef4119c94835c63abf3c3e0cb8a7b
Vladut Caliman <vladut.caliman@student.unitbv.ro>
2024-11-03 19:13:52 +0200

    Implementing game start function(1).

--- Eter/Eter/Game.cpp
+++ Eter/Eter/Game.cpp
@@ -1,9 +1,5 @@
 #include "Game.h"
 
-void Game::start()
-{
-
-}
 
 gameboard& Game::getGameBoard()
 {
@@ -12,10 +8,29 @@ gameboard& Game::getGameBoard()
 
 const Player& Game::getPlayer1() const
 {
-	return m_players[1];
+	return m_players[0];
 }
 
 const Player& Game::getPlayer2() const
 {
-	return m_players[2];
+	return m_players[1];
+}
+
+void Game::start()
+{
+	//ADD THIS AS PARAMETER { 1,1,2,2,3,3,4 }
+	m_players[0].getCards() = { 1,1,2,2,3,3,4 };
+	m_players[1].getCards() = { 1,1,2,2,3,3,4 };
+
+
+	std::unordered_set < std::pair<int, int>, pairHash> validPositions;
+	std::vector<std::pair<int, int>> valid = { {1,0},{0,1},{1,1},{0,0},{-1,0},{0,-1},{-1,-1},{1,-1},{-1,1} };
+	for (const auto& x : valid)
+	{
+		validPositions.insert(std::make_pair(m_gridMiddle.first + x.first, m_gridMiddle.second + x.second));
+	}
+
+
+	bool firstMove = true, forInsert = true, currentPlayer = 0;
+	int lockCase = -1;
 }
