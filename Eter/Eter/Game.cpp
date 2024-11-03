a21bfb8c7017278e12348c42ff2678b8e4e140af
Adriana-Huruba <adriana.huruba@student.unitbv.ro>
2024-11-03 17:21:40 +0200

    Added content to Game.cpp

--- Eter/Eter/Game.cpp
+++ Eter/Eter/Game.cpp
@@ -4,3 +4,18 @@ void Game::start()
 {
 
 }
+
+gameboard& Game::getGameBoard()
+{
+	return m_gameBoard;
+}
+
+const Player& Game::getPlayer1() const
+{
+	return m_players[1];
+}
+
+const Player& Game::getPlayer2() const
+{
+	return m_players[2];
+}
