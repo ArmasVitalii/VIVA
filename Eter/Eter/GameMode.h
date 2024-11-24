fd5406415e0e0e799ad55344b558ddadb2adb8f9
Vladut Caliman <vladut.caliman@student.unitbv.ro>
2024-11-24 16:43:53 +0200

    Continuing on gameMode
    1st path

--- Eter/Eter/GameMode.h
+++ Eter/Eter/GameMode.h
@@ -13,6 +13,23 @@ private:
 	bool						m_hasTimer{ 0 };
 
 public:
-	
+	bool						getIs4x4()				const;
+	const std::list<uint8_t>&	getCardList()				const;
+	uint8_t						getMaxNumberOfGames()	const;
+	bool						gethasMage()			const;
+	bool						gethasMagical()			const;
+	bool						gethasExplosions()		const;
+	bool						gethasIlusions()		const;
+	bool						gethasTimer()			const;
+
+	GameMode() = default;
+	GameMode(bool				is4x4,
+		std::list<uint8_t>	cardList,
+		uint8_t				maxNumberOfGames,
+		bool				hasMage,
+		bool				hasMagic,
+		bool				hasExplosions,
+		bool				hasIlusions,
+		bool				hasTimer);
 };
 
