f42df6fce40c95ea644f63b15fdc3643264e2217
Vladut Caliman <vladut.caliman@student.unitbv.ro>
2024-12-31 14:26:42 +0200

    Rework for Gamemode logic. 1

--- Eter/Eter/GamemodeDistributor.cpp
+++
@@ -1,7 +0,0 @@
-#include "GameModeDistributor.h"
-
-Gamemode GameModeDistributor::setGamemode(std::string_view modeName)
-{
-    AllGamemodes allModes;
-    return allModes.getGamemode(modeName);
-}
--- Eter/Eter/GamemodeDistributor.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "AllGamemodes.h"
-class GameModeDistributor
-{
-public:
-	static Gamemode setGamemode(std::string_view modeName);
-};
-
