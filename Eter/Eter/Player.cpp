ca26cfbe13c80e9dc0ef14961977f72b4e007c9f
Vladut Caliman <vladut.caliman@student.unitbv.ro>
2024-12-31 15:05:22 +0200

    Payer Update 2

--- Eter/Eter/Player.cpp
+++ Eter/Eter/Player.cpp
@@ -1,281 +1,81 @@
 #include "Player.h"
-#include "Game.h"
+#include <numeric>
+#include <algorithm>
 
-Player::Player(bool playerNumber /*,const std::list<Card>& playerCards*/) : m_playerNumber{ playerNumber }
+Player::Player(const std::reference_wrapper<const Gamemode>& gamemode, const std::reference_wrapper<const Bridge>& bridge)
+    :m_gamemode{ gamemode },
+    m_bridge{ bridge },
+    m_numberOfGamesWon{ 0 },
+    mtest_hasUsedMage{ gamemode.get().getMages().empty() },
+    mtest_hasUsedMagicPower{ gamemode.get().getMagicPowers().empty() }
 {
+    initialisePlayerCards();
 }
 
-std::list<int>& Player::getCards()
+void Player::initialisePlayerCards()
 {
-	return m_playerCards;
+    m_cards = m_gamemode.get().getCardList();
 }
 
-void Player::addCard(const Card& card)
+void Player::printPlayerCards() const
 {
-	m_playerCards.push_back(card.getValue());
+    if (m_cards.empty())
+    {
+        std::cout << "No cards to display\n";
+    }
+    else {
+        std::cout << "\n";
+        for (const auto& cardValue : m_cards)
+        {
+            std::cout << static_cast<int>(cardValue) << " ";
+        }
+    }
 }
 
-bool Player::removeCard(const Card& card)
+uint8_t& Player::getNumberOfGamesWon()
 {
-	auto it = std::find(m_playerCards.begin(), m_playerCards.end(), card.getValue());
-	if (it != m_playerCards.end())
-	{
-		m_playerCards.erase(it);
-		return true;
-	}
-	return false;
+    return m_numberOfGamesWon;
 }
 
-bool& Player::getPlayerNumber()
+void Player::incrementGamesWon()
 {
-	return m_playerNumber;
+    ++m_numberOfGamesWon;
 }
 
-std::vector<int> Player::getColumnVector()
+void Player::addCard(uint8_t card)
 {
-	return m_columnVector;
+    m_cards.push_back(card);
 }
 
-std::vector<int> Player::getRowVector()
+bool Player::removeCard(uint8_t cardValue)
 {
-	return m_rowVector;
+    auto it = std::find(m_cards.begin(), m_cards.end(), cardValue);
+    if (it != m_cards.end())
+    {
+        static_cast<int>(*it);
+        m_cards.erase(it);
+        return true;
+    }
+
+    return false;
 }
 
-void Player::printPlayableCards() const
+bool Player::hasUsedAllCards() const
 {
-	std::cout << "\n Playable values: ";
-	for (const auto& x : m_playerCards)
-	{
-		std::cout << x << " ";
-	}
-	std::cout << "\n";
+    return m_cards.empty() == true;
 }
 
-void Player::ControlledExplosion()
+bool Player::hasUsedMage() const
 {
-	//need for EXPLOSION CARD
-	std::cout << "Controlled Explosion activated!" << std::endl;
+    return mtest_hasUsedMage == true;
 }
 
-void Player::Destruction(Player& opponent)
+bool Player::hasUsedMagic() const
 {
-	std::list<int>& opponentCards = opponent.getCards();
-	int lastCardValue = opponentCards.back();
-	opponentCards.pop_back();
-	std::cout << "Distroyed opponent's last card :" << lastCardValue << "\n";
+    return mtest_hasUsedMagicPower == true;
 }
 
-void Player::Flames(Player& opponent, int playedCard)
+const Gamemode& Player::getGamemode() const
 {
-	std::cout << "The opponent's illusionhas been flipped!" << std::endl;
-	//need for ILLUSIONS
-	addCard(playedCard); 
+    return m_gamemode;
 }
-
-void Player::Lava(int number)
-{
-	//return all cards with the specified number to the respective players
-	int countCards = std::count(m_playerCards.begin(), m_playerCards.end(), number);
-
-	if (countCards >= 2)
-	{
-		for (auto it = m_playerCards.begin(); it != m_playerCards.end();)
-		{
-			if (*it == number)
-			{
-				it = m_playerCards.erase(it);
-			}
-			else
-			{
-				++it;
-			}
-		}
-		std::cout << "Lava activated! Cards with the number " << number << " are returned to their owners!" << std::endl;
-
-	}
-	else
-		std::cout << "Cannot use Lava Power! Not enough cards with the chosen number: " << number;
-	
-}
-
-void Player::FromAshes(int cardValue)
-{
-	//...
-	Card newCard(cardValue);
-	addCard(Card(cardValue));  //need a constructor for Card
-	std::cout << "Card " << cardValue << " has been played from the ashes!" << std::endl;
-}
-
-void Player::Sparks(Player& opponent, int cardValue)
-{
-	Card moveCard(cardValue);
-	if (opponent.removeCard(moveCard)) 
-	{
-		addCard(moveCard);
-		std::cout << "The opponent's covered card ("<<cardValue<<") has been moved to your deck!" << std::endl;
-	}
-	else
-	{
-		std::cout << "The specified card (" << cardValue << ") could not be found in the opponent's deck!" << std::endl;
-	}
-}
-
-void Player::Blizzard(Player& opponent, int cardValue)
-{
-	//move the last visibile card from the opponent to their hand
-	Card visibileCard(cardValue); 
-	if (opponent.removeCard(visibileCard))
-	{
-		opponent.addCard(visibileCard);
-		std::cout << "The visibile card (" << cardValue << ") has been returned to the opponent's hand!" << std::endl;
-	}
-	else
-	{
-		std::cout << "The specified card (" << cardValue << ") is not visibile or does not exist in the opponent's deck!" << std::endl;
-	}
-}
-
-void Player::Storm()
-{
-	//return all covered cards on the board to the players' hands
-	for (auto& value : m_playerCards)
-	{
-		//logic for returning covered cards to the player
-		std::cout << "Card returned to the player's hand!" << std::endl;
-	}
-}
-
-void Player::Hurricane(int row)
-{
-	std::cout << "Hurricane has shifted row " << row << "!" << std::endl;
-
-	//logic for shifting a complete row on the board 
-}
-
-void Player::Gale(int visibileCard)
-{
-	
-	std::cout << "Card" << visibileCard << " has been moved to an adjacent position!" << std::endl;
-	//move the card to an adjacent position with a smaller value
-}
-
-void Player::Mirage()
-{
-	std::cout << "The illusion has been replaced with another one!" << std::endl;
-	//logic to replace the illusion
-}
-
-void Player::StormSurge(Game& game) 
-{
-	//eliminate any stack of cards that has 2/more cards
-	int row, col;
-	std::cout << "Choose stack to eliminate (row and col): ";
-	std::cin >> row >> col;
-
-	if (row < 0 || row >= game.getGameboard().size() || col < 0 || col >= game.getGameboard()[row].size())
-	{
-		std::cout << "Invalid position of stack";
-		return;
-	}
-
-	if (game.getGameboard()[row][col].size() >= 2)
-	{
-		game.getGameboard()[row][col].pop();
-		std::cout << "Storm power activated. Stack at the positions: (" << row << ", " << col << ") was eliminated\n";
-	}
-	else
-		std::cout << "The stack must have at least 2 cards!";
-}
-
-void Player::Tide(Game &game)
-{
-	auto Gameboard = game.getGameboard();
-	int row1, col1;
-	int row2, col2;
-	std::cout << "The coordinates of the first stack of cards (row, col): ";
-	std::cin >> row1 >> col1;
-
-	std::cout << "The coordinates of the second stack of cards which you want to swap: ";
-	std::cin >> row2 >> col2;
-
-	if (row1 < 0 || row1 >= Gameboard.size() || col1 < 0 || row1 >= Gameboard[row1].size() ||
-		row2 < 0 || row2 >= Gameboard.size() || col2 < 0 || col2 >= Gameboard[row2].size())
-	{
-		std::cout << "Invalid coordinates! You cannot swap stacks.";
-		return;
-	}
-
-	std::swap(Gameboard[row1][col1], Gameboard[row2][col2]);
-	std::cout << "Tide power activated!";
-
-}
-
-void Player::Fog()
-{
-	std::cout << "Another illusion has been played!" << std::endl;
-	//need for illusions!
-}
-
-void Player::Wave()
-{
-	std::cout << "A stack has moved to an adjacent position and card has been played!" << std::endl;
-	//logic to move the stack and play a card in the new position
-}
-
-void Player::WaterVortex()
-{
-	std::cout << "Two cards have been moved into an empty space on the same row!" << std::endl;
-	//...
-}
-
-void Player::Tsunami(int row)
-{
-	std::cout << "Tsunami has restricted the opponent from placing cards on row " << row << "!" << std::endl;
-	//logic to restrict the opponent's move
-}
-
-void Player::Cascade(int row)
-{
-	std::cout << "Cards have fallen towards the edge of the row!" << std::endl;
-	//logic for the cards to "fall" to one side of the row
-}
-
-void Player::Support(int cardValue) 
-{
-	std::cout << "Card " << cardValue << " value increased by 1!" << std::endl;
-	//logic for increasing the card value
-}
-
-void Player::Earthquake()
-{
-	std::cout << "All cards with value 1 have been removed from the board!" << std::endl;
-    //logic to remove all cards with value 1
-}
-
-void Player::Crack(int  cardValue)
-{
-	std::cout << "Card " << cardValue << " value decreased by 1!" << std::endl;
-	//logic to decrease the card value
-}
-
-void Player::Borders() 
-{
-	std::cout << "A neutral card has been placed on the board!" << std::endl;
-	//logic to place a neutral card and interact with the board
-}
-
-void Player::Avalanche()
-{
-	std::cout << "Two adjacent stacks have been shifted!" << std::endl;
-	//logic to shift two adjacent stacks
-}
-
-void Player::Boulder()
-{
-	std::cout << "An illusion has been covered with a card!" << std::endl;
-	//logic for covering the illusion
-}
-
-
-
-
