a40f53fb21b54d8a01683c6f552eabbfe7c47c18
Vladut Caliman <vladut.caliman@student.unitbv.ro>
2024-12-31 15:00:36 +0200

    Added Utility Headers

---
+++ Eter/Eter/AllMages.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "AbstractMage.h"
+#include "EarthMage.h"
+#include "FireMage.h"
+#include "WaterMage.h"
+#include "AirMage.h"
+#include "DarkMage.h"
+#include "LightMage.h"
+#include "IceMage.h"
+#include "ThunderMage.h"
