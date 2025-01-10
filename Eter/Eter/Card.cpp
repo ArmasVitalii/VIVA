6a141ec3065e7ffbd71b36c76e30873f329a4c27
Vladut Caliman <vladut.caliman@student.unitbv.ro>
2025-01-10 20:18:42 +0200

    Updated Card to support Illusions.

--- Eter/Eter/Card.cpp
+++ Eter/Eter/Card.cpp
@@ -1,7 +1,7 @@
 #include "Card.h"
 
-Card::Card(uint8_t value, PlayerEnum playerID)
-	:m_cardValue{ value }, m_playerID{ playerID }
+Card::Card(uint8_t value, PlayerEnum playerID, bool isIllusion)
+	:m_cardValue{ value }, m_playerID{ playerID }, m_isIllusion{ isIllusion }
 {
 }
 
@@ -18,4 +18,20 @@ uint8_t Card::getValue() const
 PlayerEnum Card::getPlayerID() const
 {
 	return m_playerID;
+}
+
+bool Card::isIllusion() const
+{
+	return m_isIllusion;
+}
+
+void Card::discoverIllusion()
+{
+	m_hasBeenDiscovered = true;
+	m_isIllusion = false;
+}
+
+uint8_t Card::getRealValue() const
+{
+	return (m_isIllusion || m_hasBeenDiscovered) ? 1u : m_cardValue;
 }
--- Eter/Eter/Card.h
+++ Eter/Eter/Card.h
@@ -6,12 +6,16 @@ class Card
 private:
 	uint8_t		m_cardValue;
 	PlayerEnum	m_playerID;
-
+	bool		m_isIllusion;
+	bool		m_hasBeenDiscovered{ false };
 public:
-	Card(uint8_t value, PlayerEnum playerID);
+	Card(uint8_t value, PlayerEnum playerID, bool isIllusion = false);
 	bool operator==(const Card& card) const;
 
 	uint8_t		getValue() const;
 	PlayerEnum	getPlayerID() const;
+	bool		isIllusion() const;
+	void		discoverIllusion();
+	uint8_t		getRealValue() const;
 };
 
